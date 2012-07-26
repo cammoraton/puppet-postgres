a61e6cf3900f545fb572971d697dcae6cb44315d
Nick Cammorato <nick.cammorato@gmail.com>
2012-07-26 19:01:25 -0400

    Reworked pg_exec based on regular puppet exec.

--- lib/puppet/provider/pg_exec/psql.rb
+++ lib/puppet/provider/pg_exec/psql.rb
@@ -1,131 +1,65 @@
-require 'puppet/provider/postgres'
-# The difference between a user and group these days
-# is if pg_catalog.pg_roles.rolcanlogin is set to true or false
-# so we use a single provider to handle both users and groups.
-Puppet::Type.type(:pg_exec).provide(:psql, :parent => Puppet::Provider::Postgres) do
-  desc 'Provider to execute sql commands on the basis of the result of a query'
+Puppet::Type.type(:pg_exec).provide(:psql) do
+
+  desc 'Provider to add, delete, manipulate postgres databases.'
   
   commands :psql => '/usr/bin/psql'
+  commands :pgrep => '/usr/bin/pgrep'
   
-  # We don't do anything with instances for this.
-  def self.instances
+  def run
+    basecmd = []
+    basecmd << command(:psql)
+    basecmd << "-U #{@resource[:role]}" unless @resource[:role].nil?
+    basecmd << "-d #{@resource[:database]}" unless @resource[:database].nil?
     
-    block_until_ready
-
-  end
-  
-  # or Prefetch
-  def self.prefetch(resources)
-    
-  end
-  
-  # We always exist
-  def exists?
-    true
-  end
-  
-  # Getters.  Apart from our query, they always equal the resource
-  def database
-    @resource[:database]
-  end
-  
-  def query
-    @resource[:query]
-  end
-  
-  def user
-    @resource[:user]
-  end
-  
-  def exec
-    @resource[:exec]
-  end
-  
-  def type
-    @resource[:type]
-  end
-  
-  def evaluates
-    # We execute the actual query here.
-    cmd = []
-    cmd << command(:psql)
-    cmd << '-qAtc'
-    cmd << @resource[:query]
-    cmd << @resource[:database] unless @resource[:database].nil?
-    raw, status = Puppet::Util::SUIDManager.run_and_capture(cmd, @resource[:user])
-
-    if status == 0
-      return :true
-    else 
-      return :false
+    raw = nil
+    execute = true
+    unless @resource[:query].nil?
+      cmd = basecmd
+      cmd << '-qAtc'
+      
+      sqlcmd = "#{@resource[:query]}"
+      
+      cmd << sqlcmd
+      
+      raw, status = Puppet::Util::SUIDManager.run_and_capture(cmd, 'postgres')
+      warning("Got #{status} and #{raw}")
+      if status == 0
+        unless @resource[:result].nil?
+        # Compare raw to the regex and modify execute
+          execute = false
+        else
+          execute = false
+        end
+      end
     end
-  end
-  
-  def database=(should)
-    @property_hash[:database] = should
-  end
-  
-  def evaluates=(should)
-    @property_hash[:evaluates] = should
-  end
-  
-  def query=(should)
-    @property_hash[:query] = should
-  end
-  
-  def user=(should)
-    @property_hash[:user] = should
-  end
-  
-  def exec=(should)
-    @property_hash[:exec] = should
-  end
-  
-  def type=(should)
-    @property_hash[:type] = should
-  end
-  
-  # Should never be called
-  def create
-    @property_hash = {
-      :name     => @resource[:name],
-      :database => @resource[:database],
-      :evaluates  => @resource[:evaluates],
-      :user     => @resource[:user],
-      :query    => @resource[:query],
-      :exec     => @resource[:exec],
-      :type     => @resource[:type],
-      :ensure   => :present
-    }
-  end
-  
-  # Also should never be called
-  def destroy
-    @property_hash.clear
-  end
-  
-
-  
-  # Flush gets triggered if evaluates value is something other than what you're expecting
-  def flush
-   unless @property_hash.empty?
-      cmd = []
-      cmd << command(:psql)
-      case @resource[:type]
-       when :sql
-         cmd << '-qAtc'
-       when :file
-         cmd << '-qAtf'
-       else
-         fail("Unknown type #{@resource[:type]}")
-      end  
-      cmd << @resource[:exec]      
-      raw, status = Puppet::Util::SUIDManager.run_and_capture(cmd, @resource[:user])
-
+    
+    unless execute == false
+      cmd = basecmd
+      if ! @resource[:command].nil?
+        cmd << '-qAtc'
+        
+        sqlcmd = "#{@resource[:command]}"
+        
+        cmd << sqlcmd   
+      elsif ! @resource[:file].nil?
+        cmd << '-qAtf'
+        
+        sqlcmd = "#{@resource[:file]}"
+        
+        cmd << sqlcmd
+      else
+        self.fail("Nothing to do.")
+      end
+      
+      raw, status = Puppet::Util::SUIDManager.run_and_capture(cmd, 'postgres')
+      warning("Got #{status} and #{raw}")
       if status != 0
-        fail("Error executing command - #{raw}")
+        self.fail("Error executing SQL - result #{raw}")
+      else
+        @ran = true
       end
     end
   end
   
 end
+  
--- lib/puppet/type/pg_exec.rb
+++ lib/puppet/type/pg_exec.rb
@@ -1,43 +1,158 @@
 module Puppet
   newtype(:pg_exec) do
-    @doc = "Conditionally execute SQL commands"
-
-    ensurable
+    @doc = "Execute SQL commands on a postgresql server.
+    
+    This has a number of different behaviors depending on which parameters are set.
+    
+    Refreshonly behaves as it does for the exec type.  If set to true then this only does
+    anything if a dependant resource is modified.
+    
+    The next bit depends on a few factors:
+    - If query is set then it will query the SQL server as the user specified.
+      - If result is set then it will attempt to match the query to the provided expression and only execute
+        the command if it finds a match.
+      - If rows is set then it will execute if the number of rows returned from the query meets the criteria
+      - If neither of the above 2 are set, then it will execute if the PSQL command returns any code except 0.
+    - If the above conditions are met for an execution it will then execute the psql command
+      - If command is set, it will do a psql -qAtc :command
+      - If file is set, it will do a psql -qAtf :file
+    - If neither a command or a file are set, it will simply refresh.  You could use this, potentially, to
+      trigger other resources to run on the basis of DB queries.
+    - This defaults to a naked execution as the postgres user, but user and database can be specified.
+    
+    Like the exec type it inherits refreshonly from - be very, very careful with this.  
+    It's mainly included in order to be able do things like load a default dataset or install postgres plugins 
+    immediately post-server installation, or immediately after setting up a new database, 
+    with the query used as an extra layer of protection against accidental refreshes
+    (IE: you change the DB ACL or owner). "
 
     newparam(:name) do
-      desc "Name identifier of this database. This value needs to be unique."
+      desc "Name identifier of this SQL exec. This value needs to be unique."
       isnamevar
     end
-    
-    newproperty(:evaluates) do
-      desc "Do we execute if the query has a result or doesn't have a result?"
-      newvalues(:true, :false)
-      defaultto :true
+
+
+    newparam(:database) do
+      desc "Database to execute and evaluate against."
+      
+      #if value != 'postgres'
+        # Autorequire the database
+      #end
     end
     
-    # Default to the postgres user on the postgres database
-    newproperty(:user) do
-      desc "The user puppet should become to execute the query.  Defaults to postgres."
-      defaultto 'postgres'
+    newparam(:role) do
+      desc "Role to run the command as.  This is equivalent to connecting over local socket
+      as that role"
+      
+      # Require the role.
     end
     
-    newproperty(:database) do
-      desc "The database puppet should connect to."
-      defaultto 'postgres'
+    newparam(:query) do
+      desc "Query to run to determine if we should execute our command or not"
+
     end
     
-    newproperty(:query) do
-      desc "The query to evaluate.  Returning anything is treated as success."
+    newparam(:file) do
+      desc "Execute SQL command(s) out of a file.  Useful for plugins which place install sql"
+      
+      # Check for the file - but don't autorequire it.
     end
     
-    newproperty(:exec) do
-      desc "What to execute.  Either a SQL statment of a file path."
+    newparam(:command) do
+      desc "Raw SQL to run."
+     
+    end
+    
+    newparam(:result) do
+      desc "Regex to match result of query against"
+    end
+    
+    newproperty(:status) do
+      # We default to running things on a return code of 1
+      # Which is returned if a table doesn't exist.
+      defaultto "0"
+      
+       # Make output a bit prettier
+      def change_to_s(currentvalue, newvalue)
+        if ! @ran
+          return "evaluated but not run"
+        else
+          return "evaluated and ran"
+        end
+      end
+      
+      # First verify that all of our checks pass.
+      def retrieve
+        if @resource.check_all_attributes
+          return :notrun
+        else
+          return self.should
+        end
+      end
+      
+      def sync
+        provider.run
+      end
+    end
+    
+    # The following was pulled out of Puppet::Exec in order to provide refreshonly as an option.
+    # Every time this resource refreshes it queries the database, so you probably want to be able
+    # to confine that behavior via that mechanism.
+        
+    # Create a new check mechanism.  It's basically just a parameter that
+    # provides one extra 'check' method.
+    def self.newcheck(name, options = {}, &block)
+      @checks ||= {}
+
+      check = newparam(name, options, &block)
+      @checks[name] = check
     end
 
-    newproperty(:type) do
-      desc "What query is.  Either a SQL statement or a file."
-      newvalues(:sql, :file)
-      defaultto :sql
+    def self.checks
+      @checks.keys
+    end
+    
+    newcheck(:refreshonly) do
+      desc "The command should only be run as a
+        refresh mechanism for when a dependent object is changed."
+
+      newvalues(:true, :false)
+
+      # We always fail this test, because we're only supposed to run
+      # on refresh.
+      def check(value)
+        # We have to invert the values.
+        if value == :true
+          false
+        else
+          true
+        end
+      end
+    end
+    
+    # Verify that we pass all of the checks.  The argument determines whether
+    # we skip the :refreshonly check, which is necessary because we now check
+    # within refresh
+    def check_all_attributes(refreshing = false)
+      self.class.checks.each { |check|
+        next if refreshing and check == :refreshonly
+        if @parameters.include?(check)
+          val = @parameters[check].value
+          val = [val] unless val.is_a? Array
+          val.each do |value|
+            return false unless @parameters[check].check(value)
+          end
+        end
+      }
+
+      true
+    end
+
+    # Run the command
+    def refresh
+      if self.check_all_attributes(true)
+        
+      end
     end
   end
 end
